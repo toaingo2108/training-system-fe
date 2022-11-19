8629c0b267e7dea259501ac938a725781f3d0371
toaingo2181 <nqtoai2181@gmail.com>
2022-11-19 12:10:38 +0700

    push code

--- src/pages/Course/create.jsx
+++ src/pages/Course/create.jsx
@@ -1,5 +1,13 @@
 import { LoadingButton } from '@mui/lab';
-import { Grid, TextField } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Paper,
+  TextField
+} from '@mui/material';
 import React, { useState } from 'react';
 import MyContainer from '../../components/container';
 
@@ -33,8 +41,78 @@ const CourseCreate = () => {
 
   return (
     <MyContainer>
-      <Grid container>
-        <Grid item>
+      <Grid container spacing={3}>
+        <Grid
+          item
+          container
+          flexDirection='column'
+          xs={12}
+          sm={8}
+          spacing={2}
+          // justifyContent='space-between'
+        >
+          <Grid item>
+            <TextField
+              name={fieldForm.name}
+              onChange={handleChangeCreateForm}
+              label='Tên khóa học'
+              size='small'
+              variant='outlined'
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              name={fieldForm.imgLink}
+              onChange={handleChangeCreateForm}
+              label='Image URL'
+              size='small'
+              variant='outlined'
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              name={fieldForm.description}
+              onChange={handleChangeCreateForm}
+              label='Mô tả'
+              size='small'
+              variant='outlined'
+              fullWidth
+              required
+            />
+          </Grid>
+        </Grid>
+        <Grid item container xs={12} sm={4} spacing={2} justifyContent='center'>
+          <Grid item className='w-full'>
+            <img
+              className='w-full h-full rounded-lg object-cover'
+              src={createForm.imgLink}
+              alt={createForm.name}
+            />
+          </Grid>
+        </Grid>
+        <Grid item container>
+          <LoadingButton
+            loading={creating}
+            size='large'
+            variant='contained'
+            onClick={() => handleSubmitCreateForm()}
+          >
+            Tạo khóa học
+          </LoadingButton>
+        </Grid>
+      </Grid>
+      <Card sx={{ height: '100%', width: '100%', borderRadius: 3 }}>
+        <CardMedia
+          component='img'
+          alt={createForm.name}
+          height="100"
+          image={createForm.imgLink}
+        />
+        <CardContent>
           <TextField
             name={fieldForm.name}
             onChange={handleChangeCreateForm}
@@ -44,16 +122,35 @@ const CourseCreate = () => {
             fullWidth
             required
           />
-        </Grid>
-        <LoadingButton
-          loading={creating}
-          size='large'
-          variant='contained'
-          onClick={() => handleSubmitCreateForm()}
-        >
-          Tạo khóa học
-        </LoadingButton>
-      </Grid>
+          <TextField
+            name={fieldForm.imgLink}
+            onChange={handleChangeCreateForm}
+            label='Image URL'
+            size='small'
+            variant='outlined'
+            fullWidth
+            required
+          />
+          <TextField
+            name={fieldForm.description}
+            onChange={handleChangeCreateForm}
+            label='Mô tả'
+            size='small'
+            variant='outlined'
+            fullWidth
+            required
+          />
+          <TextField
+            name={fieldForm.description}
+            onChange={handleChangeCreateForm}
+            label='Mô tả'
+            size='small'
+            variant='outlined'
+            fullWidth
+            required
+          />
+        </CardContent>
+      </Card>
     </MyContainer>
   );
 };

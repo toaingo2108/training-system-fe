befdd076da477201629553d5ca6ff781639244f9
toaingo2181 <nqtoai2181@gmail.com>
2022-12-16 18:13:08 +0700

    update component user

--- src/components/user/index.jsx
+++ src/components/user/index.jsx
@@ -1,9 +1,11 @@
 import { AccountCircle } from '@mui/icons-material';
 import { Avatar, Button, IconButton, Menu, MenuItem } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/auth';
-import ModalUser from './modal';
+
+const modal = document.getElementById('modal-user');
+const button = document.getElementById('button-user');
 
 const User = () => {
   const navigate = useNavigate();
@@ -18,56 +20,82 @@ const User = () => {
   const handleClose = () => {
     setIsOpenMenu(false);
   };
+
+  const handleClick = (event) => {
+    console.log(event.target);
+    handleClose(false);
+  };
+
+  const handleToggleModal = (event) => {
+    event.preventDefault();
+    setIsOpenMenu(!isOpenMenu);
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', handleClick);
+    return () => {
+      window.removeEventListener('click', handleClick);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   if (!user) {
     return;
   }
   return (
     <div className='relative'>
       <IconButton
-        fullWidth
+        id='button-user'
         size='small'
         color='inherit'
-        onClick={() => setIsOpenMenu(!isOpenMenu)}
+        onClick={handleToggleModal}
       >
         {user?.imgLink ? (
-          <Avatar sx={{ width: 24, height: 24 }} src={user?.imgLink} />
+          <Avatar
+            sx={{ width: 24, height: 24 }}
+            src={user?.imgLink}
+            imgProps={{}}
+          />
         ) : (
           <AccountCircle sx={{ width: 24, height: 24 }} />
         )}
       </IconButton>
-      {isOpenMenu && (
-        <div
-          className={`absolute px-6 py-4 bg-white rounded-lg opacity-0 right-0 transition-all animate-[fadeIn]`}
-          style={{
-            boxShadow: '0 -4px 32px rgb(0 0 0 / 20%)'
-          }}
-        >
-          <div className='py-2 hover:scale-105 cursor-pointer flex items-center'>
-            <Avatar src={user?.imgLink} />
-            <div className='ml-4 flex flex-col'>
-              <b className='whitespace-nowrap'>{`${user?.firstName || ''} ${
-                user?.lastName || ''
-              }`}</b>
-              <i className='text-xs'>@{user?.username || ''}</i>
-            </div>
-          </div>
-          <hr />
-          <div className='py-2 hover:scale-105 cursor-pointer'>
-            Trang cá nhân
-          </div>
-          <hr />
-          <div className='py-2 hover:scale-105 cursor-pointer'>Cài đặt</div>
-          <div
-            className='py-2 hover:scale-105 cursor-pointer'
-            onClick={() => {
-              logout();
-              navigate('/login');
-            }}
-          >
-            Đăng xuất
+      <div
+        id='modal-user'
+        className={`absolute px-6 py-4 bg-white rounded-lg  right-0 transition-all animate-[fadeIn] ${
+          isOpenMenu ? 'opacity-1 visible translate-y-2' : 'opacity-0 invisible'
+        }`}
+        style={{
+          boxShadow: '0 -4px 32px rgb(0 0 0 / 20%)'
+        }}
+      >
+        <div className='transition-all py-2  cursor-pointer flex items-center'>
+          <Avatar src={user?.imgLink} />
+          <div className='ml-4 flex flex-col'>
+            <b className='whitespace-nowrap'>{`${user?.firstName || ''} ${
+              user?.lastName || ''
+            }`}</b>
+            <i className='text-xs'>@{user?.username || ''}</i>
           </div>
         </div>
-      )}
+        <hr />
+        <div className='transition-all py-2 hover:bg-gray-100 cursor-pointer'>
+          Trang cá nhân
+        </div>
+        <hr />
+        <div className='transition-all py-2 hover:bg-gray-100 cursor-pointer'>
+          Cài đặt
+        </div>
+        <div
+          className='transition-all py-2 hover:bg-gray-100 cursor-pointer'
+          onClick={() => {
+            logout();
+            navigate('/login');
+          }}
+        >
+          Đăng xuất
+        </div>
+      </div>
       {/* <Menu
       id='menu-appbar'
       anchorEl={anchorEl}

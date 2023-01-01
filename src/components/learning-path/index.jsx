c62505996ef662f4f1380d930eddac1376b2071f
toaingo2181 <nqtoai2181@gmail.com>
2023-01-02 00:03:23 +0700

    learning path

---
+++ src/components/learning-path/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LearningPath = () => {
+  return (
+    <div className='rounded-xl h-40 bg-gradient-to-r from-sky-300 to-indigo-300'>
+      LearningPath
+    </div>
+  );
+};
+
+export default LearningPath;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,46 @@
-import React, { useEffect } from 'react';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import { useEffect } from 'react';
+import MyContainer from '../../components/container';
+import LearningPath from '../../components/learning-path';
 
 const Home = () => {
   useEffect(() => {
     document.title = 'Trang chủ';
   }, []);
 
-  return <>HOME</>;
+  return (
+    <MyContainer>
+      <div>
+        <div className='font-black text-2xl mb-4'>Learning path</div>
+        <Splide
+          options={{
+            perPage: 4,
+            // arrows: false,
+            // pagination: false,
+            drag: 'free',
+            gap: '1rem'
+          }}
+          aria-labelledby='learning-path-slide'
+        >
+          <SplideSlide>
+            <LearningPath />
+          </SplideSlide>
+          <SplideSlide>
+            <LearningPath />
+          </SplideSlide>
+          <SplideSlide>
+            <LearningPath />
+          </SplideSlide>
+          <SplideSlide>
+            <LearningPath />
+          </SplideSlide>
+          <SplideSlide>
+            <LearningPath />
+          </SplideSlide>
+        </Splide>
+      </div>
+    </MyContainer>
+  );
 };
 
 export default Home;

214d0713d4ff1ea561d3d77bbc708b0067260046
toaingo2181 <nqtoai2181@gmail.com>
2023-01-09 01:55:34 +0700

    update delete role

--- src/clients/role.jsx
+++ src/clients/role.jsx
@@ -8,6 +8,14 @@ class RoleClient extends BaseClient {
   async createRole({ name }) {
     return super.callApi('post', '/role', { name });
   }
+
+  async updateRole({ id, name }) {
+    return super.callApi('put', `/role/${id}`, { name });
+  }
+
+  async deleteRole({ id }) {
+    return super.callApi('delete', `/role/${id}`, {});
+  }
 }
 
 export const roleClient = () => {
--- src/pages/role/index.jsx
+++ src/pages/role/index.jsx
@@ -1,31 +1,54 @@
 import { DataGrid } from '@mui/x-data-grid';
 import MyContainer from '../../components/container';
-import { Grid, Box, TextField, Button } from '@mui/material';
+import { Grid, Box, TextField, Button, IconButton } from '@mui/material';
 import { useEffect } from 'react';
 import { roleClient } from '../../clients/role';
 import CustomNoRows from '../../components/customs/no-rows';
 import { useLoading } from '../../hooks/loading';
 import { useToast } from '../../hooks/toast';
 import { useRoles } from '../../hooks/roles';
-
-const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'name',
-    headerName: 'Vai trò',
-    width: 400
-  }
-];
+import { EditRounded, RemoveCircleRounded } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 export default function Role() {
   // hooks
   const loading = useLoading();
   const toast = useToast();
+  const navigate = useNavigate();
 
   // constants
 
   // states
   const [roles, setRoles] = useRoles();
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'name',
+      headerName: 'Vai trò',
+      width: 350,
+      editable: true
+    },
+    {
+      field: 'action',
+      headerName: 'Thao tác',
+      width: 100,
+      sortable: false,
+      filterable: false,
+      renderCell: ({ row }) => {
+        return (
+          <div>
+            <IconButton onClick={() => handleUpdateRole(row)}>
+              <EditRounded />
+            </IconButton>
+            <IconButton onClick={() => handleDeleteRole(row)}>
+              <RemoveCircleRounded color='error' />
+            </IconButton>
+          </div>
+        );
+      }
+    }
+  ];
 
   // methods
   const handleCreateRole = async (event) => {
@@ -42,6 +65,33 @@ export default function Role() {
       toast.success('Thêm thành công!');
     } else {
       toast.success('Thêm thất bại!');
+    }
+  };
+
+  const handleUpdateRole = async (role) => {
+    const resUpdateRole = await roleClient().updateRole({
+      ...role
+    });
+    if (resUpdateRole.success) {
+      toast.success('Cập nhật thành công!');
+    } else {
+      toast.success(
+        'Đã có lỗi xảy ra. Vui lòng thử lại! ' + resUpdateRole.message
+      );
+    }
+  };
+
+  const handleDeleteRole = async (role) => {
+    const resDeleteRole = await roleClient().deleteRole({
+      id: role?.id
+    });
+    if (resDeleteRole.success) {
+      toast.success('Xóa thành công!');
+      navigate(0);
+    } else {
+      toast.success(
+        'Đã có lỗi xảy ra. Vui lòng thử lại! ' + resDeleteRole.message
+      );
     }
   };
 
@@ -63,6 +113,7 @@ export default function Role() {
                 NoResultsOverlay: CustomNoRows,
                 NoRowsOverlay: CustomNoRows
               }}
+              editMode='row'
             />
           </Box>
         </Grid>

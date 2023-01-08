9e555827c48ad4017057a6319d80d9e3ae824d78
toaingo2181 <nqtoai2181@gmail.com>
2023-01-09 00:48:46 +0700

    add img learning path

--- src/components/learning-path/index.jsx
+++ src/components/learning-path/index.jsx
@@ -8,6 +8,11 @@ const LearningPath = ({ learningPath }) => {
       className='rounded-xl h-40 bg-gradient-to-r from-sky-300 to-indigo-300 p-4 flex flex-col justify-between cursor-pointer hover:to-sky-300 hover:from-indigo-300 transition-all'
       onClick={() => navigate(`/learning-path/detail/${learningPath.id}`)}
     >
+      <img
+        className='w-6 h-6 object-cover rounded-full pointer-events-none'
+        src={learningPath?.imgLink}
+        alt=''
+      />
       <div className='text-sm max-h-20 overflow-hidden'>
         {textAbstract(learningPath.description, 100)}
       </div>
